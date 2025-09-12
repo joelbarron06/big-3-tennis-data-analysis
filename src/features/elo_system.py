b56b2587373491c3d7f80b488e8ae1d5cf229982
barron-joel <joelm_barron@outlook.com>
2025-09-12 20:48:38 +0100

    basic additions to elo system

---
+++ src/features/elo_system.py
@@ -0,0 +1,30 @@
+class EloSystem:
+    def __init__(self, k_factor=32, initial_rating=1500):
+        self.ratings = {}
+        self.grass_ratings = {}
+        self.clay_ratings = {}
+        self.hard_ratings = {}
+        self.carpet_ratings = {}
+        self.k_factor = k_factor
+        self.initial_rating = initial_rating
+
+    def get_rating(self, player, surface=None):
+        if player not in self.ratings:
+            self.ratings[player] = self.initial_rating
+            self.grass_ratings[player] = self.initial_rating
+            self.clay_ratings[player] = self.initial_rating
+            self.hard_ratings[player] = self.initial_rating
+            self.carpet_ratings[player] = self.initial_rating
+        if surface == 'Grass':
+            return self.grass_ratings[player]
+        elif surface == 'Clay':
+            return self.clay_ratings[player]
+        elif surface == 'Hard':
+            return self.hard_ratings[player]
+        elif surface == 'Carpet':
+            return self.carpet_ratings[player]
+        else:
+            return self.ratings[player]
+        
+    def expected_score(self, r_a, r_b):
+        return 1 / (1 + 10**((r_b - r_a) / 400))

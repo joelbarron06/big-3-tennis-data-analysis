e16d185bae9843b79a6a866d6198f2eb4eb008df
barron-joel <joelm_barron@outlook.com>
2025-09-14 19:18:53 +0100

    updating system implmented

--- src/features/elo_system.py
+++ src/features/elo_system.py
@@ -8,7 +8,7 @@ class EloSystem:
         self.k_factor = k_factor
         self.initial_rating = initial_rating
 
-    def get_rating(self, player, surface=None):
+    def get_rating(self, player, surface=None): # returns overall and surface specific rating if surface is provided
         if player not in self.ratings:
             self.ratings[player] = self.initial_rating
             self.grass_ratings[player] = self.initial_rating
@@ -26,8 +26,52 @@ class EloSystem:
         else:
             return self.ratings[player]
         
-    def expected_score(self, r_a, r_b):
-        return 1 / (1 + 10**((r_b - r_a) / 400))
-    
-    def update_rating(self, player_a, player_b, result, surface=None):
-        ...
+    def expected_score(self, r_w, r_l): # expected score of winner
+        return 1 / (1 + 10**((r_l - r_w) / 400))
+
+    def update_rating(self, winner, loser, surface=None):
+        surfaces = ['Grass', 'Clay', 'Hard', 'Carpet']
+        k = self.k_factor
+
+        if surface in surfaces:
+            r_w, r_w_surface = self.get_rating(winner, surface)
+            r_l, r_l_surface = self.get_rating(loser, surface)
+        else:
+            r_w = self.get_rating(winner)
+            r_l = self.get_rating(loser)
+
+        # calculate winner's overall rating
+        e_w = self.expected_score(r_w, r_l)
+        e_l = 1 - e_w
+
+        r_w_new = r_w + k * (1 - e_w)  
+        r_l_new = r_l + k * (0 - e_l)
+
+        # calculate winner's surface specific rating
+        if surface in surfaces:
+            e_w_surface = self.expected_score(r_w_surface, r_l_surface)
+            e_l_surface = 1 - e_w_surface
+
+            r_w_new_surface = r_w_surface + k * (1 - e_w_surface)
+            r_l_new_surface = r_l_surface + k * (0 - e_l_surface)
+
+        # update ratings
+        self.ratings[winner] = r_w_new
+        self.ratings[loser] = r_l_new
+
+        if surface in surfaces:
+            if surface == 'Grass':
+                self.grass_ratings[winner] = r_w_new_surface
+                self.grass_ratings[loser] = r_l_new_surface
+            elif surface == 'Clay':
+                self.clay_ratings[winner] = r_w_new_surface
+                self.clay_ratings[loser] = r_l_new_surface
+            elif surface == 'Hard':
+                self.hard_ratings[winner] = r_w_new_surface
+                self.hard_ratings[loser] = r_l_new_surface
+            elif surface == 'Carpet':
+                self.carpet_ratings[winner] = r_w_new_surface
+                self.carpet_ratings[loser] = r_l_new_surface
+
+        return 
+
